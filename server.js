78cfcbcbcc4cb706b6b632433ca31e4aa36b1dde
opatel0 <ojas.dilip.patel@gmail.com>
2024-01-22 15:10:03 -0500

    installed axios

--- server.js
+++ server.js
@@ -28,27 +28,31 @@ if (process.env.ON_HEROKU === 'false') {
                         await db.User.insertMany(db.seedData.users)
                         .then(async createdData => {
                             console.log(`Added ${createdData.length} users`)
-                            db.Games.model.find({})
-                                .then(async entry => {
-                                    for (let i=593; i<1679; i+=1) {
-                                        setTimeout(() => {
-                                            console.log("Progress: "+i*100+"/167876")
-                                            db.Games.seedData(i*100)
-                                                .then(gameData => {
-                                                    for (let game of gameData.data.games) {
-                                                        db.Games.model.updateOne(
-                                                            {_id: entry[0]._id},
-                                                            { $push: {gameIds: game.game_id}}
-                                                        ).then(confirmation => {
-                                                            if (confirmation.adknowledged === false) {
-                                                                console.log(`Issue pushing game_id ${game.game_id}`)
-                                                            }
-                                                        })
-                                                    }
-                                                    console.log('Added 100 game IDs')
-                                                })
-                                        }, (i-593)*10000)
-                                    }
+                            db.Games.model.deleteMany({})
+                                .then(async () => {
+                                    console.log('Removed all contents of games collection')
+                                    db.Games.model.create({})
+                                    .then(doc => {
+                                        for (let i=0; i<1679; i+=1) {
+                                            setTimeout(() => {
+                                                console.log("Progress: "+i*100+"/167876")
+                                                db.Games.seedData(i*100)
+                                                    .then(gameData => {
+                                                        for (let game of gameData.data.games) {
+                                                            db.Games.model.updateOne(
+                                                                {_id: doc._id},
+                                                                { $push: {gameIds: game.game_id}}
+                                                            ).then(confirmation => {
+                                                                if (confirmation.adknowledged === false) {
+                                                                    console.log(`Issue pushing game_id ${game.game_id}`)
+                                                                }
+                                                            })
+                                                        }
+                                                        console.log('Added 100 game IDs')
+                                                    })
+                                            }, i*5000)
+                                        }
+                                    })
                                 })
                             await db.User.find({})
                                 .then(users => res.json(users))
@@ -60,14 +64,17 @@ if (process.env.ON_HEROKU === 'false') {
 
 // home page to show request form and lure user into inputting search
 app.get('/', (req, res) => {
-    res.render('home')
+    db.Recs.find({})
+        .then(rec => {
+            res.render('home', {rec: rec})
+    })
 })
 app.get('/about', (req, res) => {
     res.render('about')
 })
 app.use('/users', usersCtrl)
 app.use('/recommendations', recommendationsCtrl)
-app.get('*', (req, res) => res.render('404'))
+app.get('/*', (req, res) => res.render('404'))
 
 app.listen(process.env.PORT, () => {
     console.log('Express is listening to port ', process.env.PORT)
