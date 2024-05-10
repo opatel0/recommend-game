1041e824b19324e3c5eb63e14798ab3239da6f4e
opatel0 <ojas.dilip.patel@gmail.com>
2024-05-10 10:37:47 -0400

    created function for core loop of seed functionality and moved to models/game.js

--- models/game.js
+++ models/game.js
@@ -9,9 +9,29 @@ const getData = async (game_id) => {
     let link = await axios.get(`https://api.mobygames.com/v1/games/${game_id}?api_key=${process.env.MOBYGAMES_APIKEY}`)
     return link
 }
-const seedData = async (offset) => {
+const getSeedData = async (offset) => {
     let link = await axios.get(`https://api.mobygames.com/v1/games?api_key=${process.env.MOBYGAMES_APIKEY}&offset=${offset}`)
     return link
 }
+// write 100 game IDs from API to DB every 10 seconds 
+// # of times specified by count parameter
+// tested 1s, 5s, and 10s. Only 10s ran without 429 error
+const seedDB = async function (seededModel, count) {
+    for (let i=0; i<count; i+=1) {
+        setTimeout(async () => {
+            let seededGames = await getSeedData(i*100)
+            for (let game of seededGames.data.games) {
+                let gameData = await model.updateOne(
+                    {_id: seededModel._id},
+                    { $push: {gameIds: game.game_id}}
+                )
+                if (!gameData.acknowledged) {
+                    console.log(`Issue pushing game_id ${game.game_id}`)
+                }
+            }
+            console.log("Progress: "+i*100+"/167876")
+        }, i*10000)
+    }
+}
 
-export {model, getData, seedData}
+export {model, getData, getSeedData, seedDB}
--- models/index.js
+++ models/index.js
@@ -1,7 +1,7 @@
 import mongoose from 'mongoose'
 import dotenv from 'dotenv'
 import recommendationSchema from './recommendation.js'
-import { model, getData, seedData } from './game.js'
+import { model, getData, getSeedData, seedDB } from './game.js'
 import { fileURLToPath } from 'url'
 dotenv.config()
 
@@ -10,7 +10,7 @@ const db = mongoose.connection
 db.on('connected', () => console.log(`Connected to MongoDB ${db.name} at ${db.host}:${db.port}`))
 
 const Recs = mongoose.model('recommendation', recommendationSchema)
-const Games = { model, getData, seedData }
+const Games = { model, getData, getSeedData }
 
 export { Recs, Games }
 
@@ -20,21 +20,5 @@ if (process.argv[1] === fileURLToPath(import.meta.url)) {
     const deletedModel = await Games.model.deleteMany({})
     console.log('Removed all contents of games collection')
     const createdModel = await Games.model.create({})
-    for (let i=0; i<1679; i+=1) {
-        setTimeout(async () => {
-            console.log("Progress: "+i*100+"/167876")
-            let seededGames = await Games.seedData(i*100)
-            console.log(seededGames)
-            for (let game of seededGames.data.games) {
-                let gameData = await Games.model.updateOne(
-                    {_id: createdModel._id},
-                    { $push: {gameIds: game.game_id}}
-                )
-                console.log(gameData)
-                if (!gameData.acknowledged) {
-                    console.log(`Issue pushing game_id ${game.game_id}`)
-                }
-            }
-        }, i*10000)
-    }
+    seedDB(createdModel, 1679)
 }
