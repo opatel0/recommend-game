189c353ef251a2c39d0962249cd330ae61b91a7e
opatel0 <ojas.dilip.patel@gmail.com>
2024-05-10 07:29:30 -0400

    moved and updated seed route

--- models/index.js
+++ models/index.js
@@ -2,6 +2,7 @@ import mongoose from 'mongoose'
 import dotenv from 'dotenv'
 import recommendationSchema from './recommendation.js'
 import { model, getData, seedData } from './game.js'
+import { fileURLToPath } from 'url'
 dotenv.config()
 
 mongoose.connect(process.env.MONGODBURI)
@@ -12,3 +13,34 @@ const Recs = mongoose.model('recommendation', recommendationSchema)
 const Games = { model, getData, seedData }
 
 export { Recs, Games }
+
+if (process.argv[1] === fileURLToPath(import.meta.url)) {
+    await Recs.deleteMany({})
+        .then(async deletedData => {
+            console.log(`Removed ${deletedData.deletedCount} recommendations`)
+            await Games.model.deleteMany({})
+        .then(async () => {
+            console.log('Removed all contents of games collection')
+            Games.model.create({})
+        .then(doc => {
+            for (let i=0; i<1679; i+=1) {
+                setTimeout(() => {
+                    console.log("Progress: "+i*100+"/167876")
+                    Games.seedData(i*100)
+                        .then(gameData => {
+                            for (let game of gameData.data.games) {
+                                Games.model.updateOne(
+                                    {_id: doc._id},
+                                    { $push: {gameIds: game.game_id}}
+                                ).then(confirmation => {
+                                    if (confirmation.adknowledged === false) {
+                                        console.log(`Issue pushing game_id ${game.game_id}`)
+                                    }
+                                })
+                            }
+                            console.log('Added 100 game IDs')
+                        })
+                }, i*10000)
+            }
+        })})})
+}
--- server.js
+++ server.js
@@ -14,40 +14,6 @@ app.use(express.static('public'))
 app.use(express.urlencoded({extended: true}))
 app.use(methodOverride('_method'))
 
-if (process.env.ON_HEROKU === 'false') {
-    // seed route
-    app.get('/seed', async (req, res) => {
-        await Recs.deleteMany({})
-            .then(async deletedData => {
-                console.log(`Removed ${deletedData.deletedCount} recommendations`)
-                await Games.model.deleteMany({})
-            .then(async () => {
-                console.log('Removed all contents of games collection')
-                Games.model.create({})
-            .then(doc => {
-                for (let i=0; i<1679; i+=1) {
-                    setTimeout(() => {
-                        console.log("Progress: "+i*100+"/167876")
-                        Games.seedData(i*100)
-                            .then(gameData => {
-                                for (let game of gameData.data.games) {
-                                    Games.model.updateOne(
-                                        {_id: doc._id},
-                                        { $push: {gameIds: game.game_id}}
-                                    ).then(confirmation => {
-                                        if (confirmation.adknowledged === false) {
-                                            console.log(`Issue pushing game_id ${game.game_id}`)
-                                        }
-                                    })
-                                }
-                                console.log('Added 100 game IDs')
-                            })
-                    }, i*10000)
-                }
-            })})})
-    })
-}
-
 // home page to show request form and lure user into inputting search
 app.get('/', (req, res) => {
     Recs.find({})
